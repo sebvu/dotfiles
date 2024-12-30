01b20b9c54d4229682f1a8dfb61e26057059f5d8
sebvu <santosjester738@gmail.com>
2024-12-30 06:10:15 -0600

    Continue methods for catppuccinify

--- scripts/catppuccinify/color-scraper.py
+++ scripts/catppuccinify/color-scraper.py
@@ -3,71 +3,74 @@ import glob
 import os
 import json
 import re
-from collections import Counter
+
+import tables
+import methods 
 
 """
-https://regex101.com/
-
-use this
+had the wrong idea about the alpha values for rgba
+instead, find the closest rgb. then add the opacity value on to it
 """
 
-# DEFINED_PATH = "../../.config/gtk-3.0/gtk.css.bak"
-DEFINED_PATH = "cases/testcase.css"
 
-def parseColorsFromPath(path) -> Counter:
-    colors = Counter()
-    rgbVersion = dict()
+DEFINED_FILE = "../../.config/gtk-3.0/gtk.css"
+
+def parseColorsFromPath(path) -> dict:
+    colors = dict()
+
 
     # fix the regex issues
     HEXREGEX = r"#([A-Fa-f0-9]{3}|[A-Fa-f0-9]{6})(?=[\s;,)\]]|$)"
-    RGBREGEX = r"rgb\(\s*(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*,\s*(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*,\s*(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*\)"
-    RGBAREGEX = r"rgba\(\s*(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*,\s*(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*,\s*(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*,\s*([0-1]\.[0-9]{2})\s*\)"
-    HSLREGEX = r"hsl\(\s*(\d{1,3}deg),\s*(\d{1,3}%),\s*(\d{1,3}%)\s*\)"
+    RGBREGEX = r"rgb\(\s*(?P<red>25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*(,|\s)\s*(?P<green>25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*(,|\s)\s*(?P<blue>25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*\)"
+    RGBAREGEX = r"rgba\(\s*(?P<red>25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*(,|\s)\s*(?P<green>25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*(,|\s)\s*(?P<blue>25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\s*(,|\s)\s*(?P<alpha>(1|0(?:\.\d+)))\s*\)"
+    # HSLREGEX = r"" (too lazy, and not neede for my purposes at this moment)
+    # HSLAREGEX = r""
     COLORREGEX = r"(?i)\b[a-zA-Z\-]+:\s*(aliceblue|antiquewhite|aqua|aquamarine|azure|beige|bisque|black|blanchedalmond|blue|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkcyan|darkgoldenrod|darkgray|darkgreen|darkgrey|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkslategrey|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dimgrey|dodgerblue|firebrick|floralwhite|forestgreen|fuchsia|gainsboro|ghostwhite|gold|goldenrod|gray|green|greenyellow|grey|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgray|lightgreen|lightgrey|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightslategrey|lightsteelblue|lightyellow|lime|limegreen|linen|magenta|maroon|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|navy|oldlace|olive|olivedrab|orange|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|purple|red|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|silver|skyblue|slateblue|slategray|slategrey|snow|springgreen|steelblue|tan|teal|thistle|tomato|turquoise|violet|wheat|white|whitesmoke|yellow|yellowgreen)\s*;?"
 
     with open(path, "r") as file:
         for line in file:
-            hexmatch = re.findall(HEXREGEX, line)
+            hexmatch = re.finditer(HEXREGEX, line)
             if hexmatch:
                 for match in hexmatch:
-                    print(match)
-                    print(f"Found hex value: #{match}")
-                    colors[f"#{match}"] += 1
-            rgbmatch = re.findall(RGBREGEX, line)
+                    hex_code = match.group(1)
+                    colors[f"#{hex_code}"] = methods.hex_to_rgb(hex_code)
+            rgbmatch = re.finditer(RGBREGEX, line)
             if rgbmatch:
                 for match in rgbmatch:
-                    print(match)
-                    joined_output = f"rgb({', '.join(tuple(match))})"
-                    print(f"Found rgb value: {joined_output}")
-                    colors[f"{joined_output}"] += 1
-            rgbamatch = re.findall(RGBAREGEX, line)
+                    red = match.group("red")
+                    green = match.group("green")
+                    blue = match.group("blue")
+                    colors[f"rgb({red}, {green}, {blue})"] = (red, green, blue)
+            rgbamatch = re.finditer(RGBAREGEX, line)
             if rgbamatch:
                 for match in rgbamatch:
-                    print(match)
-                    joined_output = f"rgba({', '.join(tuple(match))})"
-                    print(f"Found rgba value: {joined_output}")
-                    colors[f"{joined_output}"] += 1
-            hslmatch = re.findall(HSLREGEX, line)
-            if hslmatch:
-                for match in hslmatch:
-                    print(match)
-                    joined_output = f"hsl({', '.join(tuple(match))})"
-                    print(f"Found hsl value: {joined_output}")
-                    colors[f"{joined_output}"] += 1
-            colormatch = re.findall(COLORREGEX, line)
+                    red = match.group("red")
+                    green = match.group("green")
+                    blue = match.group("blue")
+                    alpha = match.group("alpha")
+                    print(alpha)
+                    colors[f"rgba({red}, {green}, {blue}, {alpha})"] = methods.rgba_to_rgb((int(red), int(green), int(blue), float(alpha)))
+            colormatch = re.finditer(COLORREGEX, line)
             if colormatch:
                 for match in colormatch:
-                    print(match)
-                    print(f"Found color value: {match}")
-                    colors[f"{match}"] += 1
+                    color_name = match.group(1)
+                    colors[color_name] = tables.NAME_RGB_EQUIVALENT.get(color_name.lower())
     return colors
+
+def createNewReplacementFile(replaceDict):
+    with open(DEFINED_FILE, 'r') as file:
+        content = file.read()
+    for old_color, new_color in replaceDict.items():
+        content = content.replace(old_color, new_color)
+    with open("replaced-file.txt", 'w') as file:
+        file.write(content)
 
 def getPathToFile() -> str:
 
-    if DEFINED_PATH and os.path.isfile(DEFINED_PATH):
-        return DEFINED_PATH
+    if DEFINED_FILE and os.path.isfile(DEFINED_FILE):
+        return DEFINED_FILE
     else:
-        if DEFINED_PATH:
+        if DEFINED_FILE:
             print("Pre-defined path did not resolve to a valid file.\n")
 
         def complete(text, state):
@@ -89,7 +92,12 @@ def main():
     colorsDict = parseColorsFromPath(getPathToFile())
     with open("output.txt", "w") as output:
         output.write(json.dumps(colorsDict, indent=2))
-    print("Outputted to output.txt!")
+    print("Tracked all existing colors")
+
+    replaceDict = methods.find_closest_colors(colorsDict)
+
+    # createNewReplacementFile(replaceDict)
+
 
 if __name__ == "__main__":
     main()
--- scripts/catppuccinify/methods.py
+++ scripts/catppuccinify/methods.py
@@ -1,8 +1,48 @@
 # methods to convert to rgb
+from scipy.spatial import KDTree
+import json
+
+import tables
 
 def rgba_to_rgb(rgba: tuple) -> tuple:
     alpha = rgba[3]
     return (rgba[0] * alpha, rgba[1] * alpha, rgba[2] * alpha)
 
-def hex_to_rgb(hex: string) -> tuple:
+def hex_to_rgb(hex: str) -> tuple:
+    hex_array = []
+    if len(hex) == 6:
+        hex_array.append(hex[:2].upper())
+        hex_array.append(hex[2:4].upper())
+        hex_array.append(hex[4:6].upper())
+    else:
+        hex_array.append((hex[0] + hex[0]).upper())
+        hex_array.append((hex[1] + hex[1]).upper())
+        hex_array.append((hex[2] + hex[2]).upper())
 
+    red = (tables.BASE16[hex_array[0][0]] * (16**1)) + (tables.BASE16[hex_array[0][1]] * (16**0))
+    green = (tables.BASE16[hex_array[1][0]] * (16**1)) + (tables.BASE16[hex_array[1][1]] * (16**0))
+    blue = (tables.BASE16[hex_array[2][0]] * (16**1)) + (tables.BASE16[hex_array[2][1]] * (16**0))
+    return (red, green, blue)
+
+def find_closest_colors(dict_list: dict) -> dict:
+    preset_colors = list(tables.CATPPUCCIN_MOCHA.values())
+    query_colors = list(dict_list.values())
+    keys = list(dict_list.keys())
+
+    tree = KDTree(preset_colors)
+
+    closest_colors = []
+    for color in query_colors:
+        distance, index = tree.query(color)
+        closest_colors.append(preset_colors[index])
+
+    matching_pair = {}
+
+    with open("wow2.txt", "w") as file:
+        for i in range(len(closest_colors)):
+            matching_pair[keys[i]] = closest_colors[i]
+            file.write(f"\nKey Color: {keys[i]}, RGB Version rgb{query_colors[i]} ->> closest color is: rgb{closest_colors[i]} ")
+
+    print(json.dumps(matching_pair, indent=2))
+
+    return matching_pair
